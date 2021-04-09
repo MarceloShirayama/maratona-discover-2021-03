47b88a7f65cdfdb10af98696823499ffd6e65691
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-04-08 21:07:39 -0300

    💥 #02: término

--- src/routes.js
+++ src/routes.js
@@ -138,6 +138,14 @@ const Job = {
 
       return res.redirect(`/job/${jobId}`);
     },
+
+    delete(req, res) {
+      const jobId = req.params.id;
+
+      Job.data = Job.data.filter((job) => Number(job.id) !== Number(jobId));
+
+      return res.redirect('/');
+    },
   },
 
   services: {
@@ -163,6 +171,8 @@ routes.post('/job', Job.controllers.save);
 
 routes.get('/job/:id', Job.controllers.show);
 routes.post('/job/:id', Job.controllers.update);
+
+routes.post('/job/delete/:id', Job.controllers.delete);
 
 routes.get('/profile', Profile.controllers.index);
 routes.post('/profile', Profile.controllers.update);
